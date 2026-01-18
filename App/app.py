2d476fb8b0b4911afc4621ee7eb1efacd1a16776
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2026-01-18 07:34:07 +0000

    feat: Adds initial 404 and coming soon pages to fill in

--- App/app.py
+++ App/app.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 # line to prevent cache from not updating static files
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/coming_soon')
+def coming_soon_tm():
+    return render_template('coming_soon_tm.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
